7a5018234540886ddaa97680673858c766bcf069
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-11-06 15:51:24 +0100

    Hej jag heter kevin

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+const button = document.getElementById();
+button.addEventListener(`click`, event => {});
