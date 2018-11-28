03d48b97ec3c01ee352dcc18861d4a7d4e9b1fee
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-11-28 11:41:27 +0100

    onsdag 28/11

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
-
 import password from './config';
 
 const connection = new WebSocket('ws://104.248.143.87:1337');
-
-const connection = new WebSocket('ws://104.248.143.87:1337');
-
 connection.onopen = () => {
   console.log('uppkoppling started ... ');
 };
@@ -17,6 +13,15 @@ connection.onerror = error => {
 connection.onmessage = message => {
   const textEl = document.getElementById('logger');
   const obj = JSON.parse(message.data);
+  console.log(obj);
+  if (obj.type === 'message') {
+    const messagein = document.getElementById('msg1');
+    const div = document.importNode(messagein.content, true);
+    div.firstElementChild.textContent = `${obj.data.author} ${obj.data.text}`;
+    const paste = document.getElementById('paste');
+    paste.appendChild(div);
+    console.log(obj.data.text);
+  }
   if (obj.type === 'heartbeat') {
     textEl.textContent += `${obj.data},`;
   }
@@ -24,7 +29,6 @@ connection.onmessage = message => {
 connection.onclose = () => {
   console.log('upploppling nedstängd ...');
 };
-
 const btnEl = document.getElementById('btn');
 btnEl.addEventListener('click', event => {
   event.preventDefault();
