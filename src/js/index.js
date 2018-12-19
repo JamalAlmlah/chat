0bd9d1385026f548c3859f3a8868971707f42bac
Jamal Al Mlah <9907jaal@res.ludvika.intra>
2018-12-19 11:44:08 +0100

    klart

--- src/js/index.js
+++ src/js/index.js
@@ -14,10 +14,34 @@ connection.onmessage = message => {
   const textEl = document.getElementById('logger');
   const obj = JSON.parse(message.data);
   console.log(obj);
+
+  if (obj.type === 'history') {
+    obj.data.forEach(messagehistory => {
+      console.log(messagehistory);
+      const messagein = document.getElementById('msg1');
+      const div = document.importNode(messagein.content, true);
+
+      const time = new Date(obj.data.time).toString();
+
+      div.firstElementChild.textContent = `${time.substr(0, 23)} ${messagehistory.author} ${
+        messagehistory.text
+      }`;
+      div.firstElementChild.style = `color: ${messagehistory.color}`;
+
+      const paste = document.getElementById('paste');
+      paste.appendChild(div);
+      console.log(obj.data.text);
+    });
+  }
   if (obj.type === 'message') {
     const messagein = document.getElementById('msg1');
     const div = document.importNode(messagein.content, true);
-    div.firstElementChild.textContent = `${obj.data.author} ${obj.data.text}`;
+
+    const time = new Date(obj.data.time).toString();
+
+    div.firstElementChild.textContent = `${time.substr(0, 23)} ${obj.data.author} ${obj.data.text}`;
+    div.firstElementChild.style = `color: ${obj.data.color}`;
+
     const paste = document.getElementById('paste');
     paste.appendChild(div);
     console.log(obj.data.text);
@@ -29,8 +53,7 @@ connection.onmessage = message => {
 connection.onclose = () => {
   console.log('upploppling nedstängd ...');
 };
-const btnEl = document.getElementById('btn');
-btnEl.addEventListener('click', event => {
+const eventhandler = event => {
   event.preventDefault();
   const textinput = document.getElementById('msg');
   const obj = {
@@ -40,4 +63,14 @@ btnEl.addEventListener('click', event => {
   };
   const jsonObj = JSON.stringify(obj);
   connection.send(jsonObj);
+  textinput.value = '';
+};
+const btnEl = document.getElementById('btn');
+window.addEventListener('keypress', event => {
+  console.log(event.code);
+
+  if (event.code === 'Enter') {
+    eventhandler(event);
+  }
 });
+btnEl.addEventListener('click', eventhandler);
